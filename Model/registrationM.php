98b9d5fa993dc35354c184d8b457669a3fb81aa4
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-10-10 16:37:36 +0200

    améliorations de l'inscription

--- Model/registrationM.php
+++ Model/registrationM.php
@@ -34,7 +34,7 @@
     {
         $dbLink = dbConnect();
 
-        $query = 'SELECT pseudo, email FROM `User` WHERE pseudo = \'' . $s_pseudo . '\' AND email = \'' . $s_email . '\'';
+        $query = 'SELECT pseudo FROM `User` WHERE pseudo = \'' . $s_pseudo . '\'';
 
         if (!($dbResult = mysqli_query($dbLink, $query))) {
             echo 'Erreur de requête<br/>';
@@ -47,10 +47,27 @@
 
         $result = $dbResult->fetch_assoc();
 
-        if ($result['pseudo'] == NULL AND $result['email'] == NULL)
-            return 0;
-        else if ($result['pseudo'] != NULL)
+        if ($result['pseudo'] != NULL)
             return 1;
         else
-            return 2;
+        {
+            $query = 'SELECT email FROM `User` WHERE email = \'' . $s_email . '\'';
+            if (!($dbResult = mysqli_query($dbLink, $query))) {
+                echo 'Erreur de requête<br/>';
+                //Affiche le type d'erreur.
+                echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+                //Affiche la requête envoyée.
+                echo 'Requête : ' . $query . '<br/>';
+                exit();
+            }
+
+            $result = $dbResult->fetch_assoc();
+
+            if ($result['email'] != NULL)
+                return 2;
+            else
+                return 0;
+
+        }
+
     }
