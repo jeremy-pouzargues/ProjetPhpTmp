c0521e08521843c981240baf690c17ec0ff225af
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-10-10 17:22:31 +0200

    améliorations de l'inscription réussi il reste le profil et les changements de données à faire

---
+++ Controller/profileC.php
@@ -0,0 +1,9 @@
+<?php
+
+
+    $s_action = $_GET['action'];
+
+    if ($s_action == 'chPseudo')
+    {
+        $s_pseudo == $_GET['chPseudo'];
+    }
--- View/profileV.php
+++ View/profileV.php
@@ -20,8 +20,12 @@
 
     <p>
         Votre pseudo : <?php echo $_SESSION['user']->getMyPseudo(); ?> <br/>
+        <input type="text" name="chPseudo" placeholder="Changer votre pseudo" /> <br/>
+        <input type="submit" name="action" value="chPseudo"/> <br/>
 
         Votre Email : <?php echo $_SESSION['user']->getMyEmail(); ?> <br/>
+        <input type="text" name="chEmail" placeholder="Changer votre email" /> <br/>
+        <input type="submit" name="action" value="chEmail"/> <br/>
 
     </p>
 
