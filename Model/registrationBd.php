154d5be35fe0708b722e85b6e5862626eeae7a8c
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-10-10 11:12:56 +0200

    debug

--- Model/registrationBd.php
+++ Model/registrationBd.php
@@ -8,6 +8,10 @@
                                     "191346_admin",
                                 "jullazone")
             or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+
+        mysqli_select_db($dbLink,"projet-iut-info_projetphp")
+        or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
         return $dbLink;
     }
 
