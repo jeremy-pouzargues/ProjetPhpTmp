c6942aae1f6428b35cef3fc6ad86bb4ac54af1db
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-10-16 10:52:22 +0200

    mise à jour de l'inscription

--- View/registrationV.php
+++ View/registrationV.php
@@ -29,11 +29,25 @@
 
 
     <form action="../Controller/registrationC.php" method="post">
+        <input type="text" name="Surname" placeholder="Prénom" /> <br/>
+        <input type="text" name="Name" placeholder="Nom" /> <br/>
         <input type="text" name="Pseudo" placeholder="Pseudo" /> <br/>
-        <input type="text" name="Email" placeholder="E-Mail"/> <br/>
+        <input type="email" name="Email" placeholder="E-Mail"/> <br/>
+        <input type="date" name="Birth" placeholder="Date de naissance"/> <br/>
         <input type="password" name="Pwd" placeholder="Mot de passe"/> <br/>
         <input type="password" name="Pwd2" placeholder="Verification du mot de passe"/> <br/>
         <?php echo '<strong>' . $s_error . '</strong><br/>' ?>
+        <select name="Gender">
+            <option value="Man">
+                Homme
+            </option>
+            <option value="Woman">
+                Femme
+            </option>
+            <option value="None">
+                Non spécifié
+            </option>
+        </select>
         <input type="radio" name="Gender" value="Homme"/> Homme
         <input type="radio" name="Gender"value="Femme"/> Femme <br/>
         <input type="submit" name="action" value="Registration"/> <br/>
